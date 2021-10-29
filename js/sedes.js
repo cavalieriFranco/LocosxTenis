b0e8dbd29867a33a68602908023f7e4b5df87ddd
cavalieriFranco <88116098+cavalieriFranco@users.noreply.github.com>
2021-10-29 19:58:38 -0300

    Update sedes.js

--- js/sedes.js
+++ js/sedes.js
@@ -1,4 +1,4 @@
-let informacion = "https://github.com/cavalieriFranco/LocosxTenis/blob/master/js/sedes.js";
+let informacion = "http://127.0.0.1:5500/json/datos.json";
 
 $.getJSON(informacion, function (respuesta,estado){
     if(estado === "success"){
